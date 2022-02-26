783ad2d46fefe543f854a87b0c7aa1cc9715e81c
Chip Castle <chip@chipcastle.com>
2022-02-26 15:52:12 -0600

    [nvim] Add mappings for my telescope extensions: code-fence, software-licenses

--- .config/nvim/lua/chip/telescope.lua
+++ .config/nvim/lua/chip/telescope.lua
@@ -8,4 +8,7 @@ utils.map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 utils.map('n', '<leader>fi', '<cmd>Telescope file_browser<cr>')
 
 require('telescope').load_extension('telescope-code-fence')
-vim.cmd('nnoremap <leader>cf <cmd>Telescope telescope-code-fence find')
+utils.map('n', '<leader>cf', '<cmd>Telescope telescope-code-fence find<cr>')
+
+require('telescope').load_extension('software-licenses');
+utils.map('n', '<leader>sl', '<cmd>Telescope software-licenses find<cr>')
