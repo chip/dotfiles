58f2b2e84e43ced587082eddd2523100db367826
Chip Castle <chip@chipcastle.com>
2022-06-21 22:47:34 -0500

    [nvim] Reformat plugins list

--- .config/nvim/lua/chip/plugins.lua
+++ .config/nvim/lua/chip/plugins.lua
@@ -1,36 +1,25 @@
 require "paq" {
-  "savq/paq-nvim", -- Paq manages itself
-  "neovim/nvim-lspconfig",
-  "hrsh7th/cmp-nvim-lsp", -- autocompletion with nvim-cmp
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-vsnip", -- For vsnip users.
-  "hrsh7th/vim-vsnip",
-  "williamboman/nvim-lsp-installer",
-  "nvim-lua/plenary.nvim", -- For navigating files with nvim-telescope
-  "nvim-telescope/telescope.nvim",
-  {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}, -- Optional plugins for nvim-telescope
-  "kyazdani42/nvim-web-devicons", -- If you want to have icons in your statusline choose one of these
-  "onsails/lspkind-nvim",
-  "numToStr/Comment.nvim", -- For code comments
-  "Olical/conjure",
-  "tpope/vim-dispatch",
-  "clojure-vim/vim-jack-in",
-  "radenling/vim-dispatch-neovim",
-  "bhurlow/vim-parinfer", -- Parenthesis mgt for Clojure and other LISP"s
-  "Leocardoso94/clojure-snippets", -- VS Code snippets for Clojure
-  "ryanoasis/vim-devicons", -- Add icons to start screen
-  "nvim-lualine/lualine.nvim", -- Add status bar
-  "rafcamlet/nvim-luapad", -- Lua REPL
-  "sbulav/nredir.nvim", -- Redirect the output of a Vim or external command into a scratch buffer; in LUA
-  "andrejlevkovitch/vim-lua-format", -- automatically format lua code
-  "williamboman/nvim-lsp-installer",
-  "szw/vim-maximizer",
-  "nvim-telescope/telescope-github.nvim",
-  "hkupty/impromptu.nvim",
-  "lewis6991/impatient.nvim",
+    "savq/paq-nvim", -- Paq manages itself
+    "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp", -- autocompletion with nvim-cmp
+    "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp", "hrsh7th/cmp-vsnip", -- For vsnip users.
+    "hrsh7th/vim-vsnip", "williamboman/nvim-lsp-installer",
+    "nvim-lua/plenary.nvim", -- For navigating files with nvim-telescope
+    "nvim-telescope/telescope.nvim",
+    {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}, -- Optional plugins for nvim-telescope
+    "kyazdani42/nvim-web-devicons", -- If you want to have icons in your statusline choose one of these
+    "onsails/lspkind-nvim", "numToStr/Comment.nvim", -- For code comments
+    "Olical/conjure", "tpope/vim-dispatch", "clojure-vim/vim-jack-in",
+    "radenling/vim-dispatch-neovim", "bhurlow/vim-parinfer", -- Parenthesis mgt for Clojure and other LISP"s
+    "Leocardoso94/clojure-snippets", -- VS Code snippets for Clojure
+    "ryanoasis/vim-devicons", -- Add icons to start screen
+    "nvim-lualine/lualine.nvim", -- Add status bar
+    "rafcamlet/nvim-luapad", -- Lua REPL
+    "sbulav/nredir.nvim", -- Redirect the output of a Vim or external command into a scratch buffer; in LUA
+    "andrejlevkovitch/vim-lua-format", -- automatically format lua code
+    "williamboman/nvim-lsp-installer", "szw/vim-maximizer",
+    "nvim-telescope/telescope-github.nvim", "hkupty/impromptu.nvim",
+    "lewis6991/impatient.nvim", "ckipp01/stylua-nvim"
 }
 vim.opt.runtimepath:append("~/code/telescope-code-fence.nvim")
 vim.opt.runtimepath:append("~/code/telescope-software-licenses.nvim")
