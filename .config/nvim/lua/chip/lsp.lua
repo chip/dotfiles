cead48cbdd6fddabcb5f81de06d2131af7c7279b
Chip Castle <chip@chipcastle.com>
2022-07-28 16:03:07 -0500

    [nvim] Add gopls

--- .config/nvim/lua/chip/lsp.lua
+++ .config/nvim/lua/chip/lsp.lua
@@ -88,3 +88,38 @@ require'lspconfig'.vuels.setup {}
 require'lspconfig'.tailwindcss.setup {}
 require'lspconfig'.eslint.setup {}
 require'lspconfig'.tsserver.setup {}
+
+-- <golang>
+util = require "lspconfig/util"
+
+lspconfig.gopls.setup {
+  cmd = {"gopls", "serve"},
+  filetypes = {"go", "gomod"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
+}
+
+function OrgImports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
+
+vim.cmd('autocmd BufWritePre *.go lua OrgImports(1000)')
+-- </golang>
