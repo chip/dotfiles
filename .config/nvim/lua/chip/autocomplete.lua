d81b58ed1d2660e530329b6cf253ff5dfbcd4ec3
Chip Castle <chip@chipcastle.com>
2023-03-05 12:48:02 -0600

    [nvim] Update cmp mappings per README

--- .config/nvim/lua/chip/autocomplete.lua
+++ .config/nvim/lua/chip/autocomplete.lua
@@ -79,9 +79,9 @@ cmp.setup({
         end
       end
     }),
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete({}),
     ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ['<C-e>'] = cmp.mapping({i = cmp.mapping.abort(), c = cmp.mapping.close()}),
     -- Accept currently selected item. If none selected, `select` first item.
