754de4dcc35213f8a3bd145198a57a98a48406be
Chip Castle <chip@chipcastle.com>
2023-03-05 12:51:43 -0600

    [nvim] Remove conjure, unused plugins

--- .config/nvim/lua/chip/plugins.lua
+++ .config/nvim/lua/chip/plugins.lua
@@ -15,24 +15,14 @@ require "paq" {
     "kyazdani42/nvim-web-devicons", -- If you want to have icons in your statusline choose one of these
     "onsails/lspkind-nvim",
     "numToStr/Comment.nvim", -- For code comments
-    "Olical/conjure",
-    "tpope/vim-dispatch",
-    "clojure-vim/vim-jack-in",
-    "radenling/vim-dispatch-neovim",
     "bhurlow/vim-parinfer", -- Parenthesis mgt for Clojure and other LISP"s
-    "Leocardoso94/clojure-snippets", -- VS Code snippets for Clojure
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     "ryanoasis/vim-devicons", -- Add icons to start screen
     "nvim-lualine/lualine.nvim", -- Add status bar
-    "sbulav/nredir.nvim", -- Redirect the output of a Vim or external command into a scratch buffer; in LUA
     "andrejlevkovitch/vim-lua-format", -- automatically format lua code
-    "szw/vim-maximizer",
-    "nvim-telescope/telescope-github.nvim",
-    "hkupty/impromptu.nvim",
     "lewis6991/impatient.nvim",
     "ckipp01/stylua-nvim",
     "fatih/vim-go",
-    "iamcco/markdown-preview.nvim",
 }
 vim.opt.runtimepath:append("~/code/telescope-code-fence.nvim")
 vim.opt.runtimepath:append("~/code/telescope-software-licenses.nvim")
