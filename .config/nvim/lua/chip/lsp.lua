5a37633a15c959419d82a0c9320a676da92a6ff3
Chip Castle <chip@chipcastle.com>
2023-06-27 13:07:15 -0500

    [nvim] Add nvim-cmp capabilities for lsp, loop for lspconfig targets

--- .config/nvim/lua/chip/lsp.lua
+++ .config/nvim/lua/chip/lsp.lua
@@ -1,3 +1,66 @@
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("mason").setup()
 
 local lspconfig = require 'lspconfig'
@@ -42,48 +105,33 @@ local on_attach = function(_, bufnr)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-require'lspconfig'.clojure_lsp.setup {
-  on_attach = on_attach,
-  flags = {debounce_text_changes = 500}
-}
-
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require'lspconfig'.lua_ls.setup {
-  on_attach = on_attach,
-  flags = {debounce_text_changes = 150},
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'}
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true)
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {enable = false}
-    }
+local lua_settings = {
+  Lua = {
+    runtime = {
+      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+      version = 'LuaJIT',
+      -- Setup your lua path
+      path = runtime_path
+    },
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global
+      globals = {'vim'}
+    },
+    workspace = {
+      -- Make the server aware of Neovim runtime files
+      library = vim.api.nvim_get_runtime_file("", true)
+    },
+    -- Do not send telemetry data containing a randomized but unique identifier
+    telemetry = {enable = false}
   }
 }
 
-require'lspconfig'.bashls.setup {}
-require'lspconfig'.vuels.setup {}
-require'lspconfig'.tailwindcss.setup {}
-require'lspconfig'.eslint.setup {}
-require'lspconfig'.tsserver.setup {}
-
 -- <golang>
-util = require "lspconfig/util"
+local util = require "lspconfig/util"
 
 lspconfig.gopls.setup {
   cmd = {"gopls", "serve"},
@@ -97,6 +145,9 @@ lspconfig.gopls.setup {
       staticcheck = true,
     },
   },
+  on_attach = on_attach,
+  flags = {debounce_text_changes = 500},
+  capabilities = capabilities,
 }
 
 function OrgImports(wait_ms)
@@ -117,10 +168,17 @@ end
 vim.cmd('autocmd BufWritePre *.go lua OrgImports(1000)')
 -- </golang>
 
--- Racket
-require'lspconfig'.racket_langserver.setup{
-  on_attach = on_attach,
-}
-
-
-require'lspconfig'.solargraph.setup {}
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'racket_langserver', 'tsserver', 'eslint', 'tailwindcss', 'vuels', 'bashls', 'lua_ls', 'solargraph', 'clojure_lsp' }
+for _, lsp in ipairs(servers) do
+  local settings = {}
+  if lsp == 'lua_ls' then
+    settings = lua_settings
+  end
+  require'lspconfig'[lsp].setup {
+    on_attach = on_attach,
+    flags = {debounce_text_changes = 500},
+    capabilities = capabilities,
+    settings = settings,
+  }
+end
