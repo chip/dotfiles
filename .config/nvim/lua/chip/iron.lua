da9e56059343c67dc0dce86310f1810baa5007da
Chip Castle <chip@chipcastle.com>
2023-12-11 15:14:37 -0600

    [nvim] Update iron key mappings

--- .config/nvim/lua/chip/iron.lua
+++ .config/nvim/lua/chip/iron.lua
@@ -41,7 +41,7 @@ iron.setup {
 }
 
 -- iron also has a list of commands, see :h iron-commands for all available commands
-vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
-vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
-vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
-vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+vim.keymap.set('n', '<space>ir', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>is', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>if', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>ih', '<cmd>IronHide<cr>')
