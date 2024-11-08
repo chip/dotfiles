6c904c46600b97c8c3b7db68adc08ce7c805e37e
Chip Castle <chip@chipcastle.com>
2024-11-08 11:04:19 -0600

    Disable rubocop, trouble, telescope-software-licenses

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,9 +1,10 @@
 return {
-	-- { "folke/trouble.nvim", enabled = false },
-	{ "RRethy/vim-illuminate", enabled = false },
-	-- { "folke/flash.nvim", enabled = false },
-	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
-	{ "echasnovski/mini.indentscope", enabled = false },
-	{ "lewis6991/gitsigns.nvim", enabled = false },
-	{ "folke/todo-comments.nvim", opts = { signs = false } },
+  { "RRethy/vim-illuminate", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "lewis6991/gitsigns.nvim", enabled = false },
+  { "folke/todo-comments.nvim", opts = { signs = false } },
+  { "ggandor/leap.nvim", enabled = false },
 }
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,27 +1,18 @@
-local lspconfig = require("lspconfig")
-
 vim.opt.signcolumn = "yes"
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "ruby",
-	callback = function()
-		vim.lsp.start({
-			autostart = true,
-			name = "rubocop",
-			cmd = { "bundle", "exec", "rubocop", "--lsp" },
-		})
-	end,
-})
 
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		ruby_ls = {
-			autostart = true,
-			cmd = { "chruby (cat .ruby-version) && ~/.local/share/nvim/mason/packages/ruby-lsp/bin/ruby-lsp" },
-			init_options = {
-				formatter = "auto",
-			},
-			settings = {},
-		},
-	},
+  "neovim/nvim-lspconfig",
+  opts = {
+    ruby_ls = {
+      autostart = true,
+      cmd = { "chruby (cat .ruby-version) && ~/.local/share/nvim/mason/packages/ruby-lsp/bin/ruby-lsp" },
+      init_options = {
+        formatter = "auto",
+      },
+      settings = {},
+    },
+    rubocop = {
+      enabled = false,
+    },
+  },
 }
